b70ff2b624002e255b4474ca218c950ec8c0abfd
bmppa <65456094+bmppa@users.noreply.github.com>
2024-08-09 09:56:30 -0400

    Back to Keys

--- modules/module-2/main.tf
+++ modules/module-2/main.tf
@@ -1,21 +1,10 @@
-/*terraform {
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.27"
     }
   }
-}*/
-
-terraform { 
-  cloud { 
-    
-    organization = "pcs-nym" 
-
-    workspaces { 
-      name = "awsgoat" 
-    } 
-  } 
 }
 
 provider "aws" {
